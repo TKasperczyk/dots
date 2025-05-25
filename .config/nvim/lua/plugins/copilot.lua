44c2be884b3c32001c2d2c5450b4a1003af3e589
Tomasz Kasperczyk <14127992+TKasperczyk@users.noreply.github.com>
2025-05-25 16:40:11 +0200

    Animations, smear cursor and copilot in blink.cmp

---
+++ .config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,10 @@
+return {
+    "zbirenbaum/copilot.lua",
+    opts = {
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+    },
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = true,
+}
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,48 +1,74 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    enabled = false,
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = "VeryLazy",
     config = function()
-        require('lualine').setup {
+        -- Custom Lualine component to show attached language server
+        local clients_lsp = function()
+            local bufnr = vim.api.nvim_get_current_buf()
+
+            local clients = vim.lsp.get_clients()
+            if next(clients) == nil then
+                return ""
+            end
+
+            local c = {}
+            for _, client in pairs(clients) do
+                table.insert(c, client.name)
+            end
+            return " " .. table.concat(c, "|")
+        end
+
+        require("lualine").setup({
             options = {
-                icons_enabled = true,
-                theme = 'auto',
-                component_separators = { left = '', right = '' },
-                section_separators = { left = '', right = '' },
-                disabled_filetypes = {
-                    statusline = {},
-                    winbar = {},
-                },
-                ignore_focus = {},
-                always_divide_middle = true,
-                always_show_tabline = true,
-                globalstatus = false,
-                refresh = {
-                    statusline = 100,
-                    tabline = 100,
-                    winbar = 100,
-                }
+                component_separators = "",
+                section_separators = { left = "", right = "" },
+                disabled_filetypes = { "alpha", "Outline" },
             },
             sections = {
-                lualine_a = { 'mode' },
-                lualine_b = { 'branch', 'diff', 'diagnostics' },
-                lualine_c = { 'filename' },
-                lualine_x = { 'encoding', 'fileformat', 'filetype' },
-                lualine_y = { 'progress' },
-                lualine_z = { 'location' }
+                lualine_a = {
+                    { "mode", separator = { left = " ", right = "" }, icon = "" },
+                },
+                lualine_b = {
+                    {
+                        "filetype",
+                        icon_only = true,
+                        padding = { left = 1, right = 0 },
+                    },
+                    "filename",
+                },
+                lualine_c = {
+                    {
+                        "branch",
+                        icon = "",
+                    },
+                    {
+                        "diff",
+                        symbols = { added = " ", modified = " ", removed = " " },
+                        colored = false,
+                    },
+                },
+                lualine_x = {
+                    {
+                        "diagnostics",
+                        symbols = { error = " ", warn = " ", info = " ", hint = " " },
+                        update_in_insert = true,
+                    },
+                },
+                lualine_y = { clients_lsp },
+                lualine_z = {
+                    { "location", separator = { left = "", right = " " }, icon = "" },
+                },
             },
             inactive_sections = {
-                lualine_a = {},
+                lualine_a = { "filename" },
                 lualine_b = {},
-                lualine_c = { 'filename' },
-                lualine_x = { 'location' },
+                lualine_c = {},
+                lualine_x = {},
                 lualine_y = {},
-                lualine_z = {}
+                lualine_z = { "location" },
             },
-            tabline = {},
-            winbar = {},
-            inactive_winbar = {},
-            extensions = {}
-        }
-    end
+            extensions = { "toggleterm", "trouble" },
+        })
+    end,
 }
