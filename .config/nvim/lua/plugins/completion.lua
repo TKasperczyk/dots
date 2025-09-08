025fb43725477a8977f36bfcfb71e5626445b97b
Tomasz Kasperczyk <kontakt@tomaszkasperczyk.name>
2025-09-08 18:46:30 +0200

    Fixed blink copilot

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -2,18 +2,22 @@ return {
     "saghen/blink.cmp",
     dependencies = {
         "rafamadriz/friendly-snippets",
-        "giuxtaposition/blink-cmp-copilot",
+        "fang2hou/blink-copilot",
     },
     build = "cargo build --release",
     version = "v0.*",
+    
+    config = function(_, opts)
+        -- Setup blink-copilot first
+        require('blink-copilot').setup({})
+        require('blink.cmp').setup(opts)
+    end,
 
     opts = {
         keymap = { preset = "default" },
 
         appearance = {
-            -- keep using nvim-cmp as frontend, but include Copilot as a source
-            use_nvim_cmp_as_default = true,
-            nerd_font_variant       = "mono",
+            nerd_font_variant = "mono",
         },
 
         signature = { enabled = true },
@@ -23,10 +27,10 @@ return {
             default = { "lsp", "path", "snippets", "buffer", "copilot" },
             providers = {
                 copilot = {
-                    name         = "copilot",
-                    module       = "blink-cmp-copilot",
+                    name = "copilot",
+                    module = "blink-copilot",
                     score_offset = 100,
-                    async        = true,
+                    async = true,
                 },
             },
         },
